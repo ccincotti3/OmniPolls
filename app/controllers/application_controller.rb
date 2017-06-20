bf7f80f639ccf0aba02213ba8295c608939d31a9
ccincotti3 <ccincotti3@gmail.com>
2017-06-19 23:16:46 -0400

    Application controller functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_methods :logged_in?, :current_user
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout
+    @current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
