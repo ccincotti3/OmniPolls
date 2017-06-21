3d1ce23a5a9bfa8f230a9d67b71aa42475ab0425
ccincotti3 <ccincotti3@gmail.com>
2017-06-20 21:53:33 -0400

    Add name columns to users db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620022153) do
+ActiveRecord::Schema.define(version: 20170621014350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170620022153) do
     t.string   "session_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
   end
 
 end
---
+++ frontend/components/session/new_user_form_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { logIn } from '../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = ({session}, ownProps) => ({
+  loggedIn: Boolean(session.currentUser),
+  errors: session.errors
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logIn: (user) => dispatch(logIn(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
